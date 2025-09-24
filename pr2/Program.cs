a58467b3e417534c6453831401df2fc2c36b1e37
Svetlana <svetlananovak777@gmail.com>
2025-09-24 10:50:07 +0300

    First commit

---
+++ pr2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Jerry");
+A();
+Example();
+Thread.Sleep(500);
+Example();
+Thread.Sleep(500);
+Example();
+Thread.Sleep(500);
+A();
+
+void Example()
+{
+    Console.WriteLine("A");
+    Console.WriteLine("B");
+    Console.WriteLine("C");
+}
+
+void A()
+{
+    Console.WriteLine("Функция А");
+    B();
+    C();
+}
+
+void C()
+{
+    Console.WriteLine("Функция С");
+}
+
+void B()
+{
+    Console.WriteLine("Функция B");
+}
+
+
+
+
